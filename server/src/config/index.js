84f75ad3c08252368fadf1e9726bc779bd4a544f
charliehay <charlie.hay@telus.com>
2019-08-24 13:30:19 -0400

    feat(hc-08): config index update

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,6 +1,6 @@
-import devConfig from 'config/app-services.development.json'
-import prodConfig from 'config/app-services.production.json'
-import globalConfig from 'config/app-services.global.json'
+import devConfig from 'config/app-services.development.json.js.js'
+import prodConfig from 'config/app-services.production.json.js.js'
+import globalConfig from 'config/app-services.global.json.js.js'
 
 const configuration = () => {
   switch (process.env.NODE_ENV) {
